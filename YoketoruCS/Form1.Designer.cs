38b51cd03b68d97873005300e501f4f278d88be7
I_wakamatsu <130728120+ichi-waka@users.noreply.github.com>
2023-07-13 13:34:15 +0900

    0713

--- YoketoruCS/Form1.Designer.cs
+++ YoketoruCS/Form1.Designer.cs
@@ -28,12 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            labelTitle = new Label();
+            buttonStart = new Button();
+            buttonTitle = new Button();
+            labelGameover = new Label();
+            labelClear = new Label();
+            labelHighScore = new Label();
+            fileSystemWatcher1 = new FileSystemWatcher();
+            labelCopyright = new Label();
+            tempPlayer = new Label();
+            tempObstacle = new Label();
+            tempItem = new Label();
+            labelTimer = new Label();
+            labelScore = new Label();
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).BeginInit();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            // 
+            // labelTitle
+            // 
+            labelTitle.AutoSize = true;
+            labelTitle.Location = new Point(363, 137);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(56, 15);
+            labelTitle.TabIndex = 0;
+            labelTitle.Text = "よけとるCS";
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(344, 247);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(125, 60);
+            buttonStart.TabIndex = 1;
+            buttonStart.Text = "Start!";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // buttonTitle
+            // 
+            buttonTitle.Location = new Point(344, 313);
+            buttonTitle.Name = "buttonTitle";
+            buttonTitle.Size = new Size(125, 60);
+            buttonTitle.TabIndex = 2;
+            buttonTitle.TabStop = false;
+            buttonTitle.Text = "Back to title";
+            buttonTitle.UseVisualStyleBackColor = true;
+            buttonTitle.Click += buttonTitle_Click;
+            // 
+            // labelGameover
+            // 
+            labelGameover.AutoSize = true;
+            labelGameover.Location = new Point(363, 195);
+            labelGameover.Name = "labelGameover";
+            labelGameover.Size = new Size(72, 15);
+            labelGameover.TabIndex = 3;
+            labelGameover.Text = "GAME OVER";
+            // 
+            // labelClear
+            // 
+            labelClear.AutoSize = true;
+            labelClear.Location = new Point(363, 82);
+            labelClear.Name = "labelClear";
+            labelClear.Size = new Size(44, 15);
+            labelClear.TabIndex = 4;
+            labelClear.Text = "CLEAR!";
+            // 
+            // labelHighScore
+            // 
+            labelHighScore.AutoSize = true;
+            labelHighScore.Location = new Point(44, 25);
+            labelHighScore.Name = "labelHighScore";
+            labelHighScore.Size = new Size(64, 15);
+            labelHighScore.TabIndex = 5;
+            labelHighScore.Text = "High score";
+            // 
+            // fileSystemWatcher1
+            // 
+            fileSystemWatcher1.EnableRaisingEvents = true;
+            fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // labelCopyright
+            // 
+            labelCopyright.AutoSize = true;
+            labelCopyright.Location = new Point(44, 60);
+            labelCopyright.Name = "labelCopyright";
+            labelCopyright.Size = new Size(62, 15);
+            labelCopyright.TabIndex = 6;
+            labelCopyright.Text = "CopyRight";
+            // 
+            // tempPlayer
+            // 
+            tempPlayer.AutoSize = true;
+            tempPlayer.Location = new Point(658, 220);
+            tempPlayer.Name = "tempPlayer";
+            tempPlayer.Size = new Size(37, 15);
+            tempPlayer.TabIndex = 7;
+            tempPlayer.Text = "(・ω・)";
+            // 
+            // tempObstacle
+            // 
+            tempObstacle.AutoSize = true;
+            tempObstacle.Location = new Point(658, 270);
+            tempObstacle.Name = "tempObstacle";
+            tempObstacle.Size = new Size(19, 15);
+            tempObstacle.TabIndex = 10;
+            tempObstacle.Text = "◇";
+            // 
+            // tempItem
+            // 
+            tempItem.AutoSize = true;
+            tempItem.Location = new Point(659, 322);
+            tempItem.Name = "tempItem";
+            tempItem.Size = new Size(19, 15);
+            tempItem.TabIndex = 11;
+            tempItem.Text = "☆";
+            // 
+            // labelTimer
+            // 
+            labelTimer.AutoSize = true;
+            labelTimer.Location = new Point(363, 25);
+            labelTimer.Name = "labelTimer";
+            labelTimer.Size = new Size(34, 15);
+            labelTimer.TabIndex = 12;
+            labelTimer.Text = "timer";
+            // 
+            // labelScore
+            // 
+            labelScore.AutoSize = true;
+            labelScore.Location = new Point(44, 137);
+            labelScore.Name = "labelScore";
+            labelScore.Size = new Size(35, 15);
+            labelScore.TabIndex = 13;
+            labelScore.Text = "score";
+            // 
+            // Form1
+            // 
+            AllowDrop = true;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(804, 451);
+            Controls.Add(labelScore);
+            Controls.Add(labelTimer);
+            Controls.Add(tempItem);
+            Controls.Add(tempObstacle);
+            Controls.Add(tempPlayer);
+            Controls.Add(labelCopyright);
+            Controls.Add(labelHighScore);
+            Controls.Add(labelClear);
+            Controls.Add(labelGameover);
+            Controls.Add(buttonTitle);
+            Controls.Add(buttonStart);
+            Controls.Add(labelTitle);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Label labelTitle;
+        private Button buttonStart;
+        private Button buttonTitle;
+        private Label labelGameover;
+        private Label labelClear;
+        private Label labelHighScore;
+        private FileSystemWatcher fileSystemWatcher1;
+        private Label tempItem;
+        private Label tempObstacle;
+        private Label tempPlayer;
+        private Label labelCopyright;
+        private Label labelTimer;
+        private Label labelScore;
     }
 }
--- YoketoruCS/Form1.cs
+++ YoketoruCS/Form1.cs
@@ -1,10 +1,285 @@
+using System.Runtime.InteropServices;
+
 namespace YoketoruCS
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
+
+        static int ScoreMax => 99999;
+        static int SpeedMax => 10;
+        static int PointRate => 100;
+
+        static int PlayerMax => 1;
+        static int ItemMax => 3;
+        static int ObstacleMax => 3;
+        static int PlayerIndex => 0;
+        static int ObstacleIndex => PlayerIndex + PlayerMax;
+        static int ItemIndex => ObstacleIndex + ObstacleMax;
+        static int LabelMax => ItemIndex + ItemMax;
+        Label[] chrLabels = new Label[LabelMax];
+        int itemCount;
+
+        int[] vx = new int[LabelMax];
+        int[] vy = new int[LabelMax];
+
+        static Random random = new Random();
+
+        enum State
+        {
+            None = -1,
+            Title,
+            Game,
+            Gameover,
+            Clear
+        }
+
+        State nextState = State.Title;
+        State currentState = State.None;
+
+        int score;
+        int timer;
+        int highScore = 100;
+        int StartTimer => 200;
+
         public Form1()
         {
             InitializeComponent();
+
+            for (int i = 0; i < LabelMax; i++)
+            {
+                chrLabels[i] = new Label();
+                chrLabels[i].AutoSize = true;
+                chrLabels[i].Top = i * 24;
+                Controls.Add(chrLabels[i]);
+
+                if (i < ObstacleIndex)
+                {
+                    chrLabels[i].Text = tempPlayer.Text;
+                    chrLabels[i].Font = tempPlayer.Font;
+                    chrLabels[i].ForeColor = tempPlayer.ForeColor;
+                }
+                else if (i < ItemIndex)
+                {
+                    chrLabels[i].Text = tempObstacle.Text;
+                    chrLabels[i].Font = tempObstacle.Font;
+                    chrLabels[i].ForeColor = tempObstacle.ForeColor;
+                }
+                else
+                {
+                    chrLabels[i].Text = tempItem.Text;
+                    chrLabels[i].Font = tempItem.Font;
+                    chrLabels[i].ForeColor = tempItem.ForeColor;
+                }
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ChangeState();
+            UpdateState();
+        }
+
+        void ChangeState()
+        {
+            if (nextState == State.None) return;
+
+            currentState = nextState;
+            nextState = State.None;
+
+            switch (currentState)
+            {
+                case State.Title:
+                    labelTitle.Visible = true;
+                    buttonStart.Visible = true;
+                    labelGameover.Visible = false;
+                    buttonTitle.Visible = false;
+                    labelClear.Visible = false;
+                    labelHighScore.Visible = true;
+                    tempPlayer.Visible = false;
+                    tempObstacle.Visible = false;
+                    tempItem.Visible = false;
+                    labelCopyright.Visible = true;
+                    break;
+
+                case State.Game:
+                    labelTitle.Visible = false;
+                    buttonStart.Visible = false;
+                    labelHighScore.Visible = false;
+                    labelCopyright.Visible = false;
+                    score = 0;
+                    itemCount = ItemMax;
+                    timer = StartTimer;
+                    for (int i = ObstacleIndex; i < vx.Length; i++)
+                    {
+                        vx[i] = random.Next(-SpeedMax, SpeedMax + 1);
+                        vy[i] = random.Next(-SpeedMax, SpeedMax + 1);
+                    }
+                    RandomObstacleAndItemPosition();
+                    break;
+
+                case State.Gameover:
+                    labelGameover.Visible = true;
+                    buttonTitle.Visible = true;
+                    labelHighScore.Visible = true;
+                    break;
+
+                case State.Clear:
+                    labelClear.Visible = true;
+                    buttonTitle.Visible = true;
+                    labelHighScore.Visible = true;
+                    break;
+            }
+        }
+
+        void UpdateState()
+        {
+            switch (currentState)
+            {
+                case State.Game:
+                    UpdateGame();
+                    break;
+            }
+        }
+
+        void UpdateGame()
+        {
+            if (GetAsyncKeyState((int)Keys.O) < 0)
+            {
+                nextState = State.Gameover;
+            }
+
+            if (GetAsyncKeyState((int)Keys.C) < 0)
+            {
+                nextState = State.Clear;
+            }
+
+            UpdateTimer();
+            UpdatePlayer();
+            UpdateObstacleAndItem();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            nextState = State.Game;
+        }
+
+        private void buttonTitle_Click(object sender, EventArgs e)
+        {
+            nextState = State.Title;
+        }
+
+        void UpdatePlayer()
+        {
+            var mpos = PointToClient(MousePosition);
+
+            chrLabels[PlayerIndex].Left = mpos.X - chrLabels[PlayerIndex].Width / 2;
+            chrLabels[PlayerIndex].Top = mpos.Y - chrLabels[PlayerIndex].Height / 2;
+        }
+
+        void UpdateObstacleAndItem()
+        {
+            for (int i = ObstacleIndex; i < chrLabels.Length; i++)
+            {
+                chrLabels[i].Left += vx[i];
+                chrLabels[i].Top += vy[i];
+
+                if (chrLabels[i].Left < 0)
+                {
+                    vx[i] = Math.Abs(vx[i]);
+                }
+                else if (chrLabels[i].Right > ClientSize.Width)
+                {
+                    vx[i] = -Math.Abs(vx[i]);
+                }
+                if (chrLabels[i].Top < 0)
+                {
+                    vy[i] = Math.Abs(vy[i]);
+                }
+                else if (chrLabels[i].Bottom > ClientSize.Height)
+                {
+                    vy[i] = -Math.Abs(vy[i]);
+                }
+
+                // 当たり判定
+                if (IsHit(chrLabels[i]))
+                {
+                    if (IsObstacle(i))
+                    {
+                        // 障害物にぶつかった
+                        nextState = State.Gameover;
+                    }
+                    else
+                    {
+                        // アイテム
+                        AddScore(timer * PointRate);
+                        itemCount--;
+                        chrLabels[i].Visible = false;
+                        if (itemCount <= 0)
+                        {
+                            nextState = State.Clear;
+                        }
+                    }
+                }
+            }
+        }
+
+        void RandomObstacleAndItemPosition()
+        {
+            for (int i = ObstacleIndex; i < chrLabels.Length; i++)
+            {
+                chrLabels[i].Left = random.Next(ClientSize.Width - chrLabels[i].Width);
+                chrLabels[i].Top = random.Next(ClientSize.Height - chrLabels[i].Height);
+            }
+        }
+
+        void UpdateTimer()
+        {
+            timer--;
+            if (timer <= 0)
+            {
+                timer = 0;
+                nextState = State.Gameover;
+            }
+
+            labelTimer.Text = $"{timer:000}";
+        }
+
+        bool IsHit(Label target)
+        {
+            var mpos = PointToClient(MousePosition);
+
+            return ((mpos.X >= target.Left)
+                && (mpos.X < target.Right)
+                && (mpos.Y >= target.Top)
+                && (mpos.Y < target.Bottom));
+        }
+
+        bool IsObstacle(int index)
+        {
+            return (index >= ObstacleIndex)
+                && (index < ItemIndex);
+        }
+
+        void AddScore(int point)
+        {
+            // 得点加算
+            score = Math.Min(score + point, ScoreMax);
+
+            UpdateScore();
+        }
+
+        void UpdateScore()
+        {
+            labelScore.Text = $"{score:00000}";
         }
     }
 }
